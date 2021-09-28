9085d84fa7970f6c1a520d9c08eee7c2e6097874
ICCesar <ICCesar@me.com>
2021-09-28 16:34:41 -0600

    Update cachematrix.R

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,50 @@
-## Put comments here that give an overall description of what your
-## functions do
+##### Assignment number 2 for R Programming. This is an R
+##### script for two functions. First function makes,
+##### stores, and  inverse the matrix. The second
+##### function takes the results from make_cash_matrix
 
-## Write a short comment describing this function
 
-makeCacheMatrix <- function(x = matrix()) {
+#### Matrix function: The function creates a matrix and the inverse is cached
+make_cache_matrix <- function(matrix_variables = matrix()) {
 
+    inverse_matrix <- NULL
+
+    set <- function(matrix) {
+            matrix_variables <<- matrix
+            inverse_matrix <<- NULL
+    }
+    set_inverse <- function(inverse) {
+        inverse_matrix <<- inverse
+    }
+
+    get <- function() {
+        matrix_variables
+    }
+    get_inverse <- function() {
+        inverse_matrix
+    }
+
+    list(set = set, get = get,
+         setInverse = setInverse,
+         getInverse = getInverse)
 }
 
+##### Calculates in verse of the matrix created by make_cache_matrix.
+cache_solve <- function(x, ...) {
 
-## Write a short comment describing this function
+    ### Return a matrix that is the inverse of 'x'
+    matrix_inverse <- x$getInverse()
 
-cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+    if(!is.null(matrix_inverse)) {
+            message("getting cached data")
+            return(matrix_inverse)
+    }
+
+    cached_matrix <- x$get()
+
+    matrix_inverse <- solve(cached_matrix) %*% cached_matrix
+
+    x$setInverse(matrix_inverse)
+
+    matrix_inverse
 }
